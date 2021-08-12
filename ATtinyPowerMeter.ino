fb8c5556c78e89963e64886c24ca6806ccab9112
moononournation <moononournation@gmail.com>
2021-08-13 06:24:01 +0800

    millis adjustment and format

--- ATtinyPowerMeter.ino
+++ ATtinyPowerMeter.ino
@@ -15,13 +15,14 @@ static float voltage = 0;
 static float current = 0;
 static float power = 0;
 static float mWh = 0;
-static unsigned long last_millis = 0;
+static float last_second = 0;
 
 static float max_v = 5.0F;
 static float max_ma = 10.0F;
 static float max_mw = 5.0F;
 
-void setup() {
+void setup()
+{
   TinyWireM.begin();
   oled.begin();
   power_mon.begin();
@@ -36,7 +37,8 @@ void setup() {
   oled.set_pos(CHART_RIGHT, 6);
   oled.print("Time:");
 
-/* figure labels, comment out to reduce distraction
+  // figure labels, comment out to reduce distraction
+  /*
   oled.set_pos(CHART_RIGHT, 0);
   oled.print("V:");
   oled.set_pos(CHART_RIGHT, 2);
@@ -48,13 +50,15 @@ void setup() {
 */
 }
 
-void loop() {
+void loop()
+{
   // collect figures
   voltage = power_mon.read_bus_voltage();
   current = power_mon.read_current();
   power = power_mon.read_power();
-  mWh += power / 1000 * (millis() - last_millis) / 3600;
-  last_millis = millis();
+  float curr_second = millis() / 1000.0;
+  mWh += power / (curr_second - last_second) / 3600;
+  last_second = curr_second;
 
   // print figures
   oled.set_pos(CHART_RIGHT, 0);
@@ -64,13 +68,21 @@ void loop() {
   oled.set_pos(CHART_RIGHT, 4);
   oled.print_float(mWh, 7);
   oled.set_pos(CHART_RIGHT, 7);
-  oled.print(last_millis / 1000);
+  oled.print(curr_second);
 
   // auto adjust graph ceiling
-  if (voltage >= max_v) max_v = voltage + CEILING_BUFFER;
-  if (current >= max_ma) max_ma = current + CEILING_BUFFER;
-  if (power > max_mw) max_mw = power + CEILING_BUFFER;
-
+  if (voltage >= max_v)
+  {
+    max_v = voltage + CEILING_BUFFER;
+  }
+  if (current >= max_ma)
+  {
+    max_ma = current + CEILING_BUFFER;
+  }
+  if (power > max_mw)
+  {
+    max_mw = power + CEILING_BUFFER;
+  }
   // plot graph
   oled.plot_value(i, 0, 2, voltage, 0.0F, max_v);
   oled.plot_value(i, 2, 2, current, 0.0F, max_ma);
@@ -78,7 +90,8 @@ void loop() {
 
   // loop graph position
   i++;
-  if (i >= CHART_RIGHT) {
+  if (i >= CHART_RIGHT)
+  {
     i = CHART_LEFT;
   }
 
